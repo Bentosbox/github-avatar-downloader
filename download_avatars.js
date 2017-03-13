6fdff535c519eec6f5d4f62bc4773d2771d4718b
Ben Li <ben_li5@yahoo.ca>
2017-03-13 23:14:58 +0000

    Added RequstUrl function

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,17 @@
 // node beer.js beer
 // process.argv[2] - name of beer
-//
-//
-//
+//url request for info
+// get request to search endpoint
+//get response
+//parse response
+// output response
 
+// request(buildsearchURL(), (err, response, body) => {
+//   if (err) {
+//     console.log("wtf",err);
+//     return false;
+//   }
+// }
 
 
 
@@ -11,8 +19,15 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = 'Bentosbox'
+var GITHUB_TOKEN = 'def816b468e50d45a946dd4e5fc0a59453f9ce05'
+// var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+// console.log(requestURL);
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+var requestURL = 'https://' + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
